1b6866ad38299c29c31472ecb4edae1fcb63d055
FelippeChemello <felippechemello@gmail.com>
2019-05-28 23:10:12 +0000

    PhpUnit

---
+++ src/Soma.php
@@ -0,0 +1,39 @@
+<?php
+namespace CodeExperts;
+
+class Soma{
+    private $num1;
+    private $num2;
+
+    public function getNum1(){
+        return $this->num1;
+    }
+
+    public function setNum1($num1){
+        $this->num1=$num1;
+    }
+
+    public function getNum2(){
+        return $this->num2;
+    }
+
+    public function setNum2($num2){
+        $this->num2=$num2;
+    }
+
+    public function somar()
+    {
+        return $this->num1 + $this->num2;
+    }
+
+    public function adicionaValor($num1){
+        if ($num1 < 0) {
+            throw new \invalidArgumentException("Parametro não deve ser negativo.");
+        }
+
+        $this->num1 += $num1;
+    }
+
+
+
+}
---
+++ tests/SomaTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodeExperts;
+use PHPUnit\Framework\TestCase;
+
+class SomaTest extends TestCase{
+
+    public function testSomaDoisNumeros(){
+        $soma = new Soma();
+        $soma->setNum1 (10);
+        $soma->setNum2 (20);
+        $this->assertEquals(30, $soma->somar());
+
+
+
+    }
+
+}
